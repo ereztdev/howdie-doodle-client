7a3c23c3fe4ca783209725038b7e0753f7549a5a
erez <erezt.dev@gmail.com>
2020-04-24 23:26:07 +0300

    init vue + sockets

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,8 @@
 <template>
   <div id="app">
+    <div id="splash">
+
+    </div>
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import io from 'socket.io-client';
+const socket = io('http://localhost:3000');
+Vue.prototype.$socket = socket;
+
+
 
 Vue.config.productionTip = false
 
