d6066c9f5a8e1c90c06a38954a1f27657aec6e8c
erez <erezt.dev@gmail.com>
2020-04-25 18:24:46 +0300

    a few updates to the pallete

--- src/components/DoodlePallete.vue
+++ src/components/DoodlePallete.vue
@@ -10,51 +10,101 @@
             <h3 @mouseover="grabberUp($event)"
                 @mouseup="grabberUp($event)"
                 @mousedown="grabberDown($event)"
-                class="my-0 p-3 card-header-title bg-primary text-capitalize text-center doodle_brushAndColor_title"
+                class="my-0 p-3 card-header-title bg-primary text-capitalize text-white-50 text-center doodle_brushAndColor_title"
             >brush & color
             </h3>
             <mdb-card-body class="text-center" cascade>
-                <p>Brush Size {{brushSize}}</p>
+                <mdb-btn block class="colorButton"
+                         @click="pickColor"
+                         :style="`
+                         background:${colorChosen}!important;
+                         color:${blackOrWhite()}
+                         `">
+                    {{showColorHex}}
+                </mdb-btn>
+                <hr/>
+                <label>Brush Size {{brushSize}}</label>
                 <input v-model="brushSize" type="range" class="custom-range" id="customRange1">
-                <p>color: {{colorChosen ? colorChosen : "#ff0000" }}</p>
-                <input class="form-control align-content-center w-100"  v-model="colorChosen" type="color" id="favcolor" name="favcolor" value="#ff0000"><br><br>
+                <div class="colorPicker--wrapper">
+                    <color-picker id="colorPicker" :value="colorChosen" @input="updateValue($event)"></color-picker>
+                    <mdb-btn @click="closeColorPicker" size="sm">close</mdb-btn>
+                </div>
             </mdb-card-body>
         </mdb-card>
     </vue-draggable-resizable>
 </template>
 
 <script>
-    import {mdbCard, mdbCardBody} from 'mdbvue';
+    import {Slider} from 'vue-color'
+
+    import {mdbCard, mdbCardBody, mdbBtn} from 'mdbvue';
     // import $ from 'jquery';
     export default {
         name: "DoodlePallete",
         data() {
             return {
-                brushSize:20,
-                colorChosen:null,
+                brushSize: 20,
+                colorChosen: '',
+                colorInt: null,
+
             }
+        },
+        computed: {
+            showColorHex() {
+                if (this.colorChosen) {
+                    return this.colorChosen.substr(1)
+                } else
+                    return `Choose Color`
+            },
         },
         components: {
             mdbCard,
             mdbCardBody,
+            mdbBtn,
+            'color-picker': Slider,
+
         },
         mounted() {
-            this.$(document).on('click', function (event) {
-            console.log(event.target);
-            })
+            // this.$(document).on('click', function (event) {
+            // console.log(event.target);
+            // })
+            this.$(".colorPicker--wrapper").hide();
+
         },
         methods: {
+            blackOrWhite() {
+                let colorNoHashThis = this.colorChosen.substr(1);
+                let colorInt = Number.parseInt(colorNoHashThis, 16);
+                console.log(colorInt);
+                this.colorInt = isNaN(colorInt) ? null : colorInt
+                return colorInt > 10000000 ? `black` : `white`;
+            },
             grabberDown(event) {
                 this.$(event.target).css({cursor: 'grabbing'})
             },
             grabberUp(event) {
                 this.$(event.target).css({cursor: 'grab'})
             },
+            pickColor() {
+                this.$(".colorPicker--wrapper").show();
+            },
+            updateValue(event) {
+                this.colorChosen = event.hex;
+            },
+            closeColorPicker() {
+                this.$(".colorPicker--wrapper").hide();
+            },
         },
     }
 </script>
 
 <style lang="scss">
+    .colorPicker--wrapper {
+        background: white;
+        position: absolute;
+        top: 60px;
+        left: 205px;
+    }
 
     .box {
         width: 500px;
