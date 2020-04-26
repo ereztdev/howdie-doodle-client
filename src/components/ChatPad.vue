2632ee6783ff855eba2d8ec950b7c4d903b74647
erez <erezt.dev@gmail.com>
2020-04-26 23:34:28 +0300

    chat is ready

--- src/components/ChatPad.vue
+++ src/components/ChatPad.vue
@@ -1,18 +1,24 @@
 <template>
     <div id="chatPad" class="chat">
-        <div class="chat__chats py-3">
-            <div class="row">
-                <div class="chat__timeAndName">
-                    <span class="chat__time">12:30</span> |
-                    <span class="chat__user">ereztdev:</span>
-                    <span class="chat__text" style="">This was amazing!</span>
-                </div>
+        <!--<div class="chat__chats py-3">
+                <ul class="chat__timeAndName">
+                    <li class="chat__time">12:30</li> |
+                    <li class="chat__user">ereztdev:</li>
+                    <li class="chat__text" style="">This was amazing!</li>
+                </ul>
+        </div>-->
+        <div class="chat__chats">
+            <div v-for="(chatRow, index) in allChat" v-bind:key="index" class="chatRowItem--wrapper">
+                <div class="chatRowItem chat__time">{{chatRow.dateTime}}</div>
+                <div class="chatRowItem chat__user"><strong>{{chatRow.name}}</strong> writes: </div>
+                <div class="chatRowItem chat__text">{{chatRow.text}}</div>
             </div>
         </div>
         <div class="chat__write_chat">
             <form @submit.prevent="sendText($event)">
                 <mdb-input class="mb-2 px-1" outline placeholder="" ariaDescribedBy="button-addon2">
-                    <mdb-btn type="submit" color="primary" size="lg" group slot="append" id="button-addon2">send</mdb-btn>
+                    <mdb-btn type="submit" color="primary" size="lg" group slot="append" id="button-addon2">send
+                    </mdb-btn>
                 </mdb-input>
             </form>
         </div>
@@ -37,19 +43,17 @@
             time() {
                 return moment().format();
             },
+            allChat(){
+                return this.$store.state.fullChat
+            },
         },
-        methods:{
-            sendText(event){
-                // console.log(event.target[0].value);
-                let singleChatTime = this.time;
-                let singleChatName = this.userName;
-                let singleChatText = event.target[0].value;
-                this.$store.commit('setSingleChat',
-                    {
-                        singleChatTime,
-                        singleChatName,
-                        singleChatText
-                    })
+        methods: {
+            sendText(event) {
+                let time = this.time;
+                let name = this.userName;
+                let text = event.target[0].value;
+                this.$store.commit('setSingleChat', {time, name, text})
+                event.target[0].value = '';
             },
         },
     }
@@ -63,24 +67,30 @@
         height: calc(34vh - 80px);
     }
 
-    .chat__time {
-        font-weight: 900;
-    }
-
-    .chat__user {
-        font-weight: 900;
-    }
-
     .chat__chats {
         overflow-y: auto;
         padding: 25px;
         height: calc(100% - 50px);
     }
-
-    .chat__text {
-        padding-left: 10px;
+    .chatRowItem--wrapper{
+        display: flex;
+        justify-content: start;
     }
-
+    .chatRowItem {
+        margin: 0 5px;
+    }
+    .chat__time {
+        line-height: 27px;
+        font-size: 12px;
+        white-space: nowrap;
+    }
+    .chat__user {
+        white-space: nowrap;
+    }
+    .chat__text {
+        overflow-wrap: break-word;
+        overflow-x: auto;
+    }
     .chat__write_chat {
         position: absolute;
         width: 100%;
--- src/store/index.js
+++ src/store/index.js
@@ -1,65 +1,65 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import moment from 'moment';
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-    canvasColor:'choose color',
-    brushSize:'20',
-    canvasElement:null,
-    ctxElement:null,
-    canvasWidth:0,
-    canvasHeight:0,
-    allVectors:[],
-    // lastVectors:[]
-    user:{
-      id:'xyz123',
-      name:'erez'
+    state: {
+        canvasColor: 'choose color',
+        brushSize: '20',
+        canvasElement: null,
+        ctxElement: null,
+        canvasWidth: 0,
+        canvasHeight: 0,
+        allVectors: [],
+        // lastVectors:[]
+        user: {
+            id: 'xyz123',
+            name: 'erez'
+        },
+        fullChat: []
     },
-    fullChat:[]
-  },
-  mutations: {
-    updateCanvasColor(state, payload){
-      state.canvasColor = payload.colorChosen
-    },
-    updateBrushSize(state, payload){
-      state.brushSize = payload.brushSize
-    },
-    setCanvasCtxAndDimensions(state, payload){
-      state.canvasElement = payload.canvasEl;
-      state.ctxElement = payload.ctxEl;
-      state.canvasWidth = payload.canvasWidth;
-      state.canvasHeight = payload.canvasHeight;
-    },
-    addVectors(state,payload){
-      if (payload.reset){
-        state.allVectors = [];
-      }else{
-        state.allVectors.push({from:payload.from,to:payload.to})
-      }
-    },
-    setUserName(state,payload){
-      if (payload.user.name && payload.user.id)
-      state.user = payload.user
+    mutations: {
+        updateCanvasColor(state, payload) {
+            state.canvasColor = payload.colorChosen
+        },
+        updateBrushSize(state, payload) {
+            state.brushSize = payload.brushSize
+        },
+        setCanvasCtxAndDimensions(state, payload) {
+            state.canvasElement = payload.canvasEl;
+            state.ctxElement = payload.ctxEl;
+            state.canvasWidth = payload.canvasWidth;
+            state.canvasHeight = payload.canvasHeight;
+        },
+        addVectors(state, payload) {
+            if (payload.reset) {
+                state.allVectors = [];
+            } else {
+                state.allVectors.push({from: payload.from, to: payload.to})
+            }
+        },
+        setUserName(state, payload) {
+            if (payload.user.name && payload.user.id)
+                state.user = payload.user
 
+        },
+        setSingleChat(state, payload) {
+            let fullTimeDate = payload.time;
+            let dateTime = moment(fullTimeDate).format('MMMM Do YYYY | HH:mm:ss');
+            let name = payload.name;
+            let text = payload.text;
+            state.fullChat.push({fullTimeDate, dateTime, name, text})
+        },
+        // updateLastVector(state,payload){
+        //   if (payload.reset){
+        //     state.lastVectors = [];
+        //   }else{
+        //     state.lastVectors.push({from:payload.from,to:payload.to})
+        //   }
+        // }
     },
-    setSingleChat(state,payload){
-      let singleChatTime = payload.singleChatTime;
-      let singleChatName = payload.singleChatName;
-      let singleChatText = payload.singleChatText;
-      state.fullChat.push({singleChatTime,singleChatName,singleChatText})
-    },
-    // updateLastVector(state,payload){
-    //   if (payload.reset){
-    //     state.lastVectors = [];
-    //   }else{
-    //     state.lastVectors.push({from:payload.from,to:payload.to})
-    //   }
-    // }
-  },
-  actions: {
-  },
-  modules: {
-  }
+    actions: {},
+    modules: {}
 })
