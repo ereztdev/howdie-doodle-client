1854694dee1e0313120126fe85f1a4486d3d0fa5
erez <erezt.dev@gmail.com>
2020-04-26 14:32:23 +0300

    mdb scss + chat WIP

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
     <div id="app">
+
         <div id="splash"></div>
         <navigation/>
         <router-view/>
--- src/components/ChatPad.vue
+++ src/components/ChatPad.vue
@@ -1,71 +1,120 @@
 <template>
-    <div id="chatPad" class="container-fluid chat">
-<div class="row">
-    <div class="col-2-xs chat__timeAndName"><span>12:30</span> | <span>ereztdev:</span></div>
-    <div class="col-10-xs chat__text"><p>This was amazing!</p></div>
-</div>
-<div class="row">
-    <div class="col-2-xs chat__timeAndName"><span>12:30</span> | <span>ereztdev:</span></div>
-    <div class="col-10-xs chat__text"><p>This was amazing!</p></div>
-</div>
-<div class="row">
-    <div class="col-2-xs chat__timeAndName"><span>12:30</span> | <span>ereztdev:</span></div>
-    <div class="col-10-xs chat__text"><p>This was amazing!</p></div>
-</div>
-<div class="row">
-    <div class="col-2-xs chat__timeAndName"><span>12:30</span> | <span>ereztdev:</span></div>
-    <div class="col-10-xs chat__text"><p>This was amazing!</p></div>
-</div>
-<div class="row">
-    <div class="col-2-xs chat__timeAndName"><span>12:30</span> | <span>ereztdev:</span></div>
-    <div class="col-10-xs chat__text"><p>This was amazing!</p></div>
-</div>
-<div class="row">
-    <div class="col-2-xs chat__timeAndName"><span>12:30</span> | <span>ereztdev:</span></div>
-    <div class="col-10-xs chat__text"><p>This was amazing!</p></div>
-</div>
-<div class="row">
-    <div class="col-2-xs chat__timeAndName"><span>12:30</span> | <span>ereztdev:</span></div>
-    <div class="col-10-xs chat__text"><p>This was amazing!</p></div>
-</div>
-<div class="row">
-    <div class="col-2-xs chat__timeAndName"><span>12:30</span> | <span>ereztdev:</span></div>
-    <div class="col-10-xs chat__text"><p>This was amazing!</p></div>
-</div>
-<div class="row">
-    <div class="col-2-xs chat__timeAndName"><span>12:30</span> | <span>ereztdev:</span></div>
-    <div class="col-10-xs chat__text"><p>This was amazing!</p></div>
-</div>
-<div class="row">
-    <div class="col-2-xs chat__timeAndName"><span>12:30</span> | <span>ereztdev:</span></div>
-    <div class="col-10-xs chat__text"><p>This was amazing!</p></div>
-</div>
-<div class="row">
-    <div class="col-2-xs chat__timeAndName"><span>12:30</span> | <span>ereztdev:</span></div>
-    <div class="col-10-xs chat__text"><p>This was amazing!</p></div>
-</div>
-
+    <div id="chatPad" class="chat">
+        <div class="chat__chats py-3">
+            <div class="row">
+                <div class="chat__timeAndName">
+                    <span class="chat__time">12:30</span> |
+                    <span class="chat__user">ereztdev:</span>
+                    <span class="chat__text" style="">This was amazing!</span>
+                </div>
+            </div>
+            <div class="row">
+                <div class="chat__timeAndName">
+                    <span class="chat__time">12:30</span> |
+                    <span class="chat__user">ereztdev:</span>
+                    <span class="chat__text" style="">This was amazing!</span>
+                </div>
+            </div>
+            <div class="row">
+                <div class="chat__timeAndName">
+                    <span class="chat__time">12:30</span> |
+                    <span class="chat__user">ereztdev:</span>
+                    <span class="chat__text" style="">This was amazing!</span>
+                </div>
+            </div>
+            <div class="row">
+                <div class="chat__timeAndName">
+                    <span class="chat__time">12:30</span> |
+                    <span class="chat__user">ereztdev:</span>
+                    <span class="chat__text" style="">This was amazing!</span>
+                </div>
+            </div>
+            <div class="row">
+                <div class="chat__timeAndName">
+                    <span class="chat__time">12:30</span> |
+                    <span class="chat__user">ereztdev:</span>
+                    <span class="chat__text" style="">This was amazing!</span>
+                </div>
+            </div>
+            <div class="row">
+                <div class="chat__timeAndName">
+                    <span class="chat__time">12:30</span> |
+                    <span class="chat__user">ereztdev:</span>
+                    <span class="chat__text" style="">This was amazing!</span>
+                </div>
+            </div>
+            <div class="row">
+                <div class="chat__timeAndName">
+                    <span class="chat__time">12:30</span> |
+                    <span class="chat__user">ereztdev:</span>
+                    <span class="chat__text" style="">This was amazing!</span>
+                </div>
+            </div>
+            <div class="row">
+                <div class="chat__timeAndName">
+                    <span class="chat__time">12:30</span> |
+                    <span class="chat__user">ereztdev:</span>
+                    <span class="chat__text" style="">This was amazing!</span>
+                </div>
+            </div>
+            <div class="row">
+                <div class="chat__timeAndName">
+                    <span class="chat__time">12:30</span> |
+                    <span class="chat__user">ereztdev:</span>
+                    <span class="chat__text" style="">This was amazing!</span>
+                </div>
+            </div>
+            <div class="row">
+                <div class="chat__timeAndName">
+                    <span class="chat__time">12:30</span> |
+                    <span class="chat__user">ereztdev:</span>
+                    <span class="chat__text" style="">This was amazing!</span>
+                </div>
+            </div>
+        </div>
+        <div class="chat__write_chat">
+            <mdb-input class="mb-2 px-1" outline="default" placeholder="" ariaDescribedBy="button-addon2">
+                <mdb-btn color="primary" size="lg" group slot="append" id="button-addon2">send</mdb-btn>
+            </mdb-input>
+        </div>
     </div>
 </template>
 
 <script>
+    import {mdbInput, mdbBtn} from 'mdbvue';
+
     export default {
-        name: "ChatPad"
+        name: "ChatPad",
+        components:{
+            mdbBtn,mdbInput
+        },
     }
 </script>
 
 <style scoped>
     #chatPad{
+        position: relative;
         background:white;
         width: 100vw;
         height: calc(34vh - 80px);
-        padding: 40px 25px;
-        overflow: auto;
     }
-    .chat__timeAndName{
+    .chat__time{
         font-weight: 900;
     }
+    .chat__user{
+        font-weight: 900;
+    }
+    .chat__chats{
+        overflow-y: auto;
+        padding: 25px;
+        height: calc(100% - 50px);
+    }
     .chat__text{
-        padding-left: 20px;
+        padding-left: 10px;
+    }
+    .chat__write_chat{
+        position: absolute;
+        width: 100%;
+        bottom: 0;
     }
 </style>
